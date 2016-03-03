0560c12a06117113164dc02fdc57703bcffd519c
aj <anton23@gmail.com>
2016-03-02 17:04:23 -0800

    piece by piece

---
+++ app/js/ajCustom.js
@@ -0,0 +1,45 @@
+
+;(function() { 
+
+
+  $("#scrollToPortfolio").on('click', function(event){     
+    event.preventDefault();
+    var viewableOffset = $("#webPortfolio").offset().top - $(window).scrollTop();
+    $('html,body').animate({scrollTop:viewableOffset}, 1000);
+ });
+
+
+    var link = $('link')[15];  // Main CSS is 15 according to layout file.
+
+    $('#viewmode').on('click', function(e) {
+      e.preventDefault;
+      var $this = $(this), 
+          stylesheet = 'css/',
+          target = e.target || e.srcElement;
+
+      if($this.data('type')==='light' || $this.data('type') === undefined) { // Then make it dark
+        stylesheet += 'dark';
+        $this.data('type', 'dark');
+        target.className = target.className.replace('moon', 'sun');
+      } else {
+        stylesheet += 'light';
+        $this.data('type', 'light');
+        target.className = target.className.replace('sun', 'moon');
+      }
+
+      link.setAttribute('href', stylesheet + '.css');
+
+    });
+
+
+
+
+  function nextChar(c) {
+      return String.fromCharCode(c.charCodeAt(0) + 1);
+  }
+  var x = "mail" + "to" + ":";
+  var y = "anto" + nextChar('m') + (24-1) + "\u0040g" + "mai" +"l\u002Eco" + nextChar('l');
+  document.getElementById('emailako').href = x + y; 
+  // document.getElementById('emailako').innerHTML = y;
+  // console.log('mmk');
+})();
